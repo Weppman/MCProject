b5f6595ea59e51a8357283e770e4b0ee86c39675
Jarren <jarren.downward@gmail.com>
2023-06-04 17:31:03 +0200

    Add all remaining UI

--- app/src/main/java/com/example/mcproject/ListFragment.java
+++ app/src/main/java/com/example/mcproject/ListFragment.java
@@ -7,13 +7,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 public class ListFragment extends Fragment {
 
-
+    ListView listView;
+    View view;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_list, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view =inflater.inflate(R.layout.fragment_list, container, false);
+        listView = view.findViewById(R.id.livtViewList);
+        return view;
     }
 }
--- app/src/main/java/com/example/mcproject/PendingDonationsFragment.java
+++ app/src/main/java/com/example/mcproject/PendingDonationsFragment.java
@@ -7,12 +7,57 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class PendingDonationsFragment extends Fragment {
+
+    View view;
+    ListView listView;
+    ListView listViewOutgoing;
+    ArrayList<ListDetailsIncomingClass> list = new ArrayList<>();
+    ArrayList<ListDetailsIncomingClass> listOutGoing = new ArrayList<>();
+    adapterForIncomming customAdapter;
+    adapterForIncomming customAdapterOutgoing;
+    OkHttpClass ok= new OkHttpClass();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pending_donations, container, false);
+        view =  inflater.inflate(R.layout.fragment_pending_donations, container, false);
+
+        listView = (ListView) view.findViewById(R.id.listVIncoming);
+        customAdapter =new adapterForIncomming(list,getContext());
+        listView.setAdapter(customAdapter);
+
+        list.clear();
+        list.add(new ListDetailsIncomingClass("Jhon","Beans",12));
+        list.add(new ListDetailsIncomingClass("Steven","Pilk",3));
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+        listViewOutgoing = (ListView) view.findViewById(R.id.listVOutgoing);
+        customAdapterOutgoing = new adapterForIncomming(listOutGoing,getContext());
+        listViewOutgoing.setAdapter(customAdapterOutgoing);
+
+        listOutGoing.clear();
+        listOutGoing.add(new ListDetailsIncomingClass("Jhon","Beans",12));
+        listOutGoing.add(new ListDetailsIncomingClass("Steven","Pilk",3));
+
+        listViewOutgoing.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+        return view;
     }
 }
